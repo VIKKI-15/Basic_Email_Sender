f90941c706a866870d4bff00ad82f9d324bea0a1
Vicky <vickykumawat9593@gmail.com>
2023-09-29 14:45:37 +0530

    dummy

--- working_mail.js
+++ working_mail.js
@@ -1,26 +1,27 @@
-const nodemailer = require("nodemailer");
+// DUMMY CODE
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: "587",
-  secure: false,
-  auth: {
-    user: "staystrong1176@gmail.com",
-    pass: "yxijizmumkirarpx",
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   host: "smtp.gmail.com",
+//   port: "587",
+//   secure: false,
+//   auth: {
+//     user: "staystrong1176@gmail.com",
+//     pass: "AppPassword",
+//   },
+// });
 
-async function main() {
-  const info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <vicky@gmail.com>', // sender address
-    to: "icansleepforhours@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+// async function main() {
+//   const info = await transporter.sendMail({
+//     from: '"Fred Foo 👻" <vicky@gmail.com>', // sender address
+//     to: "icansleepforhours@gmail.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   });
 
-  console.log("Message sent: %s", info.messageId, info.response);
-}
+//   console.log("Message sent: %s", info.messageId, info.response);
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
